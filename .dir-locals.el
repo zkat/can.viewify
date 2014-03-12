4046908953495aa3430e84f674178750d9892077
Josh Marchán <sykopomp@sykosomatic.org>
2014-03-12 01:24:41 -0700

    Added .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((js2-basic-offset . 2)
+         (css-indent-level . 2)
+         (js-indent-level . 2)
+         (c-basic-offset . 2)
+         (indent-tabs-mode . nil)
+         (fill-column . 80))))
