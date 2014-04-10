4f1b4601730304590b9b0796b35e2a09eed0d6dc
Kat Marchán <kzm@sykosomatic.org>
2014-04-09 19:43:56 -0700

    Require can and make sure ejs gets loaded if necessary

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ module.exports = function (file, opts) {
   function end () {
     var stream = this;
     compile({ext: ext, filename: filename}, data, function(err, script) {
-      stream.queue("module.exports=can.view.preload('"+filename+"',("+script+"));");
+      stream.queue("var can=require('can');"+
+                   /ejs/i.test(ext) ? "require('can/view/ejs/ejs.js');" : ""+
+                   "module.exports=can.view.preload('"+filename+"',("+script+"));");
       stream.queue(null);
     });
   }
